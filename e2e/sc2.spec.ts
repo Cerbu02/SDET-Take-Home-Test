29a8ef799d7403598b2867429a33c1e7b73963da
Cerbu02 <109674260+Cerbu02@users.noreply.github.com>
2024-05-09 19:47:36 +0300

    first commit

--- e2e/sc2.spec.ts
+++ e2e/sc2.spec.ts
@@ -1,33 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/v1/');
+test("test", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/v1/");
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').fill("standard_user");
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.getByRole('button', { name: 'LOGIN' }).click();
-  await page.getByRole('link', { name: 'Test.allTheThings() T-Shirt (' }).click();
-  await page.getByRole('button', { name: 'ADD TO CART' }).click();
-  await page.getByRole('button', { name: '<- Back' }).click();
-  await page.locator('div').filter({ hasText: /^\$15\.99ADD TO CART$/ }).getByRole('button').click();
-  await page.getByRole('button', { name: 'REMOVE' }).nth(1).click();
-  await page.locator('#shopping_cart_container').getByRole('link').click();
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+  await page
+    .getByRole("link", { name: "Test.allTheThings() T-Shirt (" })
+    .click();
+  await page.getByRole("button", { name: "ADD TO CART" }).click();
+  await page.getByRole("button", { name: "<- Back" }).click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^\$15\.99ADD TO CART$/ })
+    .getByRole("button")
+    .click();
+  await page.getByRole("button", { name: "REMOVE" }).nth(1).click();
+  await page.locator("#shopping_cart_container").getByRole("link").click();
 
-  const itemNames = await page.$$eval('.inventory_item_name', items =>
-  items.map(item => item.textContent?.trim())
-);
+  const itemNames = await page.$$eval(".inventory_item_name", (items) =>
+    items.map((item) => item.textContent?.trim())
+  );
 
-expect(itemNames).toContain('Sauce Labs Bolt T-Shirt');
+  expect(itemNames).toContain("Sauce Labs Bolt T-Shirt");
 
-  await page.getByRole('link', { name: 'CHECKOUT' }).click();
-  
+  await page.getByRole("link", { name: "CHECKOUT" }).click();
+
   await page.locator('[data-test="firstName"]').click();
-  await page.locator('[data-test="firstName"]').fill('first name');
-  await page.locator('[data-test="firstName"]').press('Tab');
-  await page.locator('[data-test="lastName"]').fill('last name');
-  await page.locator('[data-test="lastName"]').press('Tab');
-  await page.locator('[data-test="postalCode"]').fill('800100');
-  await page.getByRole('button', { name: 'CONTINUE' }).click();
-  await page.getByRole('link', { name: 'FINISH' }).click();
+  await page.locator('[data-test="firstName"]').fill("first name");
+  await page.locator('[data-test="firstName"]').press("Tab");
+  await page.locator('[data-test="lastName"]').fill("last name");
+  await page.locator('[data-test="lastName"]').press("Tab");
+  await page.locator('[data-test="postalCode"]').fill("800100");
+  await page.getByRole("button", { name: "CONTINUE" }).click();
+  await page.getByRole("link", { name: "FINISH" }).click();
 });
--- e2e/sc3.spec.ts
+++ e2e/sc3.spec.ts
@@ -1,39 +1,44 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/v1/');
+test("test", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/v1/");
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').fill("standard_user");
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.getByRole('button', { name: 'LOGIN' }).click();
-  await page.getByRole('combobox').selectOption('lohi');
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+  await page.getByRole("combobox").selectOption("lohi");
 
-interface Product {
-  title: string;
-  price: number;
-}
-const lohiSortedProducts: Product[] = [
-  { title: 'Sauce Labs Onesie', price: 7.99 },
-  { title: 'Sauce Labs Bike Light', price: 9.99 },
-  { title: 'Sauce Labs Bolt T-Shirt', price: 15.99 },
-  { title: 'Test.allTheThings() T-Shirt (Red)', price: 15.99 },
-  { title: 'Sauce Labs Backpack', price: 29.99 },
-  { title: 'Sauce Labs Fleece Jacket', price: 49.99 }
-];
+  interface Product {
+    title: string;
+    price: number;
+  }
+  const lohiSortedProducts: Product[] = [
+    { title: "Sauce Labs Onesie", price: 7.99 },
+    { title: "Sauce Labs Bike Light", price: 9.99 },
+    { title: "Sauce Labs Bolt T-Shirt", price: 15.99 },
+    { title: "Test.allTheThings() T-Shirt (Red)", price: 15.99 },
+    { title: "Sauce Labs Backpack", price: 29.99 },
+    { title: "Sauce Labs Fleece Jacket", price: 49.99 },
+  ];
 
-const afterSortProducts: Product[] = await page.$$eval('.inventory_item', (items: Element[]) =>
-  items.map((item: Element) => ({
-    title: (item.querySelector('.inventory_item_label > a') as HTMLAnchorElement)?.textContent || '',
-    price: parseFloat((item.querySelector('.inventory_item_price') as HTMLElement)?.textContent?.replace(/\$/g, '') || '0'),
-  }))
-);
+  const afterSortProducts: Product[] = await page.$$eval(
+    ".inventory_item",
+    (items: Element[]) =>
+      items.map((item: Element) => ({
+        title:
+          (item.querySelector(".inventory_item_label > a") as HTMLAnchorElement)
+            ?.textContent || "",
+        price: parseFloat(
+          (
+            item.querySelector(".inventory_item_price") as HTMLElement
+          )?.textContent?.replace(/\$/g, "") || "0"
+        ),
+      }))
+  );
 
-console.log(afterSortProducts);
-
-for (let i = 0; i < lohiSortedProducts.length; i++) {
-  expect(afterSortProducts[i].title).toBe(lohiSortedProducts[i].title);
-  expect(afterSortProducts[i].price).toBe(lohiSortedProducts[i].price);
-}
-
- });
+  for (let i = 0; i < lohiSortedProducts.length; i++) {
+    expect(afterSortProducts[i].title).toBe(lohiSortedProducts[i].title);
+    expect(afterSortProducts[i].price).toBe(lohiSortedProducts[i].price);
+  }
+});
